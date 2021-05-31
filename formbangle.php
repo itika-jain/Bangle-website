4babdb7bd0af81302a641448570348a59edc75d6
itika jain <itikajain05@gmail.com>
2021-05-31 19:56:08 +0530

    Update formbangle.php

--- formbangle.php
+++ formbangle.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $number = $_POST['number'];
 $message = $_POST['message'];
 
-$email_from = 'info@https://itika-jain.github.io/Bangle-website/';
+$email_from = 'info@https://itika-jain.github.io/Bangle-website/contactbangle.html';
 
 $email_subject = 'New Form Submission';
 
